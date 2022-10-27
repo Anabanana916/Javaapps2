db7c540683024aa2880fdee823c17e50ccf5a8c3
Anabanana916 <ana.c.elderton@gmail.com>
2022-10-27 20:22:28 +0100

    Comment added

--- src/Week5/Song.java
+++ src/Week5/Song.java
@@ -1,13 +1,18 @@
 package Week5;
 
-//Below is an array class that determines a number of
-//objects that can be called and printed
+/**
+ * Below is an array class that determines a number ofobjects that can be called and printed
+ * @author Ana
+ */
 public class Song 
 {
     private String name;
     private String artist;
     private int numberOfPlays;
-
+    
+    /**
+    * This is the constructor that will create the name, artist and number of plays
+    */
     public Song(String name, String artist, int numberOfPlays)
     {
         this.name = name;
